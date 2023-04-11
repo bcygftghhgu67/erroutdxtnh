a275fe8c3d44ffe73737ede6b798dda32ddf0fb7
bcygftghhgu67 <124477920+bcygftghhgu67@users.noreply.github.com>
2023-04-11 21:55:10 +0000

    Delete copy.js

--- copy.js
+++
@@ -1,8 +0,0 @@
-document.body.oncopy=function(){
-if(navigator.userAgent.indexOf("MSIE")>0) {
-event.returnValue=false;
-var t=document.selection.createRange().text;
-var s="："+location.href+"";
-clipboardData.setData("Text",t+"\r\n"+s+"");
-}
-}
